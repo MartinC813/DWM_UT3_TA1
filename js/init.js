b7b86762128ab97074d3e7af31a08ed77bbfa2a4
Martín <martin.cabral@correo.ucu.edu.uy>
2024-09-11 14:26:32 -0300

    ta1

---
+++ js/init.js
@@ -0,0 +1,79 @@
+let taskModal;
+let tasks = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    initilizeTasks();
+
+    const personas = ["Persona 1", "Persona 2", "Persona 3", "Persona 4", "Persona 5"];
+    const prioridades = ["Alta", "Media", "Baja"];
+    const estados = ["backlog", "toDo", "inProgress", "blocked", "done"];
+
+    taskModal = new CreateTask(personas, prioridades, estados);
+
+    document.getElementById('modal-container').innerHTML = taskModal.toHTML();
+
+    document.getElementById("newTask").addEventListener('click', () => {
+        taskModal.openModal();
+    });
+    document.getElementById("newTaskMini").addEventListener('click', () => {
+        taskModal.openModal();
+    });
+
+    document.getElementById("saveButton").addEventListener('click', () => {
+        taskModal.saveTask();
+        document.querySelector('#modal-container .modal').classList.remove('is-active');
+    });
+    document.getElementById("cancelButton").addEventListener('click', () => {
+        taskModal.cancelTask();
+        document.querySelector('#modal-container .modal').classList.remove('is-active');
+    });
+    document.querySelector('#taskTitle').addEventListener('input', () => taskModal.validateTask());
+    document.querySelector('#taskDesc').addEventListener('input', () => taskModal.validateTask());
+});
+
+
+function initilizeTasks() {
+    let tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+
+    fetch('http://localhost:3000/api/tasks')
+        .then(response => response.json())
+        .then(data => {
+            // Combina las tareas del backend con las del localStorage
+            const finales = [...tasks, ...data];
+
+            // Elementos del DOM
+            let backLog = document.getElementById('backlog');
+            let toDo = document.getElementById('toDo');
+            let inProgress = document.getElementById('inProgress');
+            let blocked = document.getElementById('blocked');
+            let done = document.getElementById('done');
+
+            // Renderiza las tareas en función de su estado
+            finales.forEach(task => {
+                const taskObj = new Task(task.title, task.description, task.assignedTo, task.priority, task.status, task.createdAt, task.dueDate, task.id);
+                switch (taskObj.status) {
+                    case 'backlog':
+                        backLog.innerHTML += taskObj.toHTML();
+                        break;
+                    case 'toDo':
+                        toDo.innerHTML += taskObj.toHTML();
+                        break;
+                    case 'inProgress':
+                        inProgress.innerHTML += taskObj.toHTML();
+                        break;
+                    case 'blocked':
+                        blocked.innerHTML += taskObj.toHTML();
+                        break;
+                    case 'done':
+                        done.innerHTML += taskObj.toHTML();
+                        break;
+                    default:
+                        break;
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
